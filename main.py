406bf664f717dca39db26bb285cadacb4a6bb392
Gookian <bezginov.daniil@mai.ru>
2023-11-01 22:06:56 +0700

    Добавил диалог вызова команд

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-#from datetime import date
 from ftp_file_downloader import FtpFileDownloader
 from tender_reader import TenderReader
 from datetime import date
 
 import ftplib
 
-ftp = ftplib.FTP(host='95.167.245.94', timeout=10)
-ftp.login(user='free', passwd='free')
-
-ftpDownloader = FtpFileDownloader(ftp, threadsCount=6)
-
 source="/fcs_regions/Novosibirskaja_obl/notifications/"
 directory_str="./regions/Novosibirskaja_obl/"
-#ftpDownloader.downloadFiles(source, directory_str, date(2016, 1, 1), date(2023,10,24))
 
-# Чтение файлов Xml
-TenderReader.readFiles(directory_str)
+while True:
+    print("Доступные команды: \n1) download\n2) read\n3) exit")
+    commandStr = input("Введите комманду: ").lower()
+
+    if commandStr == "download":
+        ftp = ftplib.FTP(host='95.167.245.94', timeout=10)
+        ftp.login(user='free', passwd='free')
+
+        ftpDownloader = FtpFileDownloader(ftp, threadsCount=6)
+        ftpDownloader.downloadFiles(source, directory_str, date(2016, 1, 1), date(2023,10,24))
+
+    elif commandStr == "read":
+        TenderReader.readFiles(directory_str)
+
+    elif commandStr == "exit":
+        break
+
+    else:
+        print("Неизвестная каманда!")
