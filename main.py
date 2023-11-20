7d55a9a0b0b9a4d420f68da31c655ef67501967c
Gookian <bezginov.daniil@mai.ru>
2023-11-20 15:35:05 +0700

    Изменил функционал, на сохранение данных в csv

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from parsers.tender_parser import TenderParser
 from datetime import date
 from parsers.structure_tender_parser import StructureTenderParser
 from repositories.repository import Repository
+from util.dict_util import DictUtil
+from data_name_column import result_compress_dict
 
 import ftplib
+import numpy as np
 
 source="/fcs_regions/Novosibirskaja_obl/notifications/"
 directory_str="./regions/Novosibirskaja_obl/"
@@ -21,8 +24,26 @@ while True:
         ftp_downloader.download_zip(source, directory_str, date(2016, 1, 1), date(2023,11,15))
 
     elif command_str == "parse":
+        names_dict = DictUtil.dictionary_compression(result_compress_dict)
+        names_array = np.array([], dtype=str)
+        for key, value in names_dict.items():
+            names_array = np.append(names_array, key)
+
+        names_array = np.insert(names_array, 0, "typePurchase")
+        names_array = np.insert(names_array, 0, "period")
+        names_array = np.insert(names_array, 0, "zipDateStart")
+        names_array = np.insert(names_array, 0, "zipDateEnd")
+
+        print(names_array)
+
+        print("Число признаков:", len(names_array), "\n")
+
         tender_parser = TenderParser()
-        tender_parser.readFiles(directory_str)
+        tender_parser.parse(directory_str, fieldnames=names_array)
+
+    elif command_str == "parsename":
+        tender_parser = TenderParser()
+        tender_parser.get_names(directory_str)
 
     elif command_str == "parsestructure":
         structure_tender_parser = StructureTenderParser()
--- parsers/tender_parser.py
+++ parsers/tender_parser.py
@@ -1,52 +1,58 @@
 from zipfile import ZipFile
 from xml.etree import ElementTree
-from pymongo import MongoClient
 from datetime import date
 from util.date_range_parser import DataRangeParser
 from util.work_timer import WorkTimer
+from util.dict_util import DictUtil
 
 import os
+import csv
+import time
 
 class TenderParser():
     def __init__(self) -> None:
-        self.data_range_parser = DataRangeParser()
         self.work_timer = WorkTimer()
+        self.data_range_parser = DataRangeParser()
+        self.ignore_tag = ["signature", "printForm", "printFormInfo", "cryptoSigns"]
 
-        self.structure_2014_2016_list = self.init_structure_data(date(2014, 1, 1), date(2017, 1, 1))
-        self.structure_2016_2020_list = self.init_structure_data(date(2017, 1, 1), date(2021, 1, 1))
-        self.structure_2020_now_list = self.init_structure_data(date(2021, 1, 1), date(2023, 11, 15))
+    def parse_to_xml(self, element):
+        # Инициализация пустого словаря для текущего элемента
+        isArray = False
+        if element.tag.split('}')[1][-1].lower() == 's':
+            current_dict = []
+            isArray = True
+        else:
+            current_dict = {}
 
-    def init_structure_data(self, to_date: date, from_date: date) -> None:
-        client = MongoClient("localhost", 27017, maxPoolSize=50)
-        db = client.gpo_tender_db
-        collection = db.structures
-        
-        query = { 
-            "toDate": to_date.strftime('%d.%m.%Y'),
-            "fromDate": from_date.strftime('%d.%m.%Y')
-        }
+        # # Перебор атрибутов текущего элемента
+        # for key, value in element.attrib.items():
+        #     print(value)
+        #     current_dict[key] = value
 
-        result = collection.find(query)
+        # Рекурсивный перебор дочерних элементов
+        for child in element:
+            if child.tag.split('}')[1] not in self.ignore_tag:
+                # Рекурсивный вызов функции для каждого дочернего элемента
+                child_dict = self.parse_to_xml(child)
 
-        structure_list = []
+                if isArray:
+                    current_dict.append({child.tag.split('}')[1]: child_dict})
+                else:
+                    # Объединение словарей текущего элемента и его дочерних элементов
+                    current_dict.setdefault(child.tag.split('}')[1], child_dict)
 
-        for structure in result:
-            structure_list.append(structure)
+        # Если у текущего элемента нет дочерних элементов, сохраняем его текст
+        if not current_dict:
+            current_dict = str(element.text)
 
-        return structure_list
+        return current_dict
 
-    def readFiles(self, directory: str):
-        client = MongoClient("localhost", 27017, maxPoolSize=50)
-        db = client.gpo_tender_db
-        collection = db.notifications
-
+    def get_names(self, directory: str) -> None:
         path = os.fsencode(directory)
         file_count = len(os.listdir(path))
         file_index = 0
 
-        with open("notifications.json", "w", encoding="utf-8") as file:
-            file.write('[')
-            file.close()
+        old_dictionary = {}
 
         for file in os.listdir(path):
             filename = os.fsdecode(file)
@@ -55,8 +61,65 @@ class TenderParser():
                 file_index += 1
 
                 self.work_timer.start()
+
+                with ZipFile(directory + filename, "r") as myzip:
+                    file_zip_count = len(myzip.namelist())
+                    file_zip_index = 0
+
+                    for nameFileXml in myzip.namelist():
+                        startT = time.time()
+                        file_zip_index += 1
+
+                        if nameFileXml.endswith(".xml"):
+                            str_data_xml = myzip.read(nameFileXml).decode('utf-8')
+                            root = ElementTree.fromstring(str_data_xml)
+
+                            try:
+                                new_dictionary = self.parse_to_xml(root[0])
+                                old_dictionary = DictUtil.merging_dictionaries(new_dictionary, old_dictionary)
+                            except Exception as err:
+                                print(f"Unexpected {err=}, {type(err)=}")
+                                continue
+                            # single_level_dictionary = DictUtil.get_names_dict(merging_dictionaries)
+
+                            # for k in single_level_dictionary:
+                            #     if k not in names_list:
+                            #         names_list.append(k)
+
+                        finish = time.time()
+                        print(f'Просмотренно zip: {file_index}/{file_count}\t{round(file_index/file_count*100, 2)}%\t|\t{file_zip_index}/{file_zip_count}\t{round(file_zip_index/file_zip_count*100, 2)}%\t|\tВремя скачивания {self.work_timer.days} дней {self.work_timer.hours} часов {self.work_timer.minutes} минут {self.work_timer.seconds} секунд\t|\t{finish - startT} s')
+
+                self.work_timer.calculate_time(file_index, file_count)
+        print(old_dictionary)
+
+    def parse(self, directory: str, fieldnames: list) -> None:
+        path = os.fsencode(directory)
+        file_count = len(os.listdir(path))
+        file_index = 0
+
+        with open('names.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')# extrasaction="ignore", quoting=csv.QUOTE_NONNUMERIC
+            writer.writeheader()
+            csvfile.close()
+
+        for file in os.listdir(path):
+            filename = os.fsdecode(file)
+
+            if filename.endswith(".zip"):
+                file_index += 1
+
+                self.work_timer.start()
+
                 date_range = self.data_range_parser.parse(filename)
-                # if date_range.start >= date(2014, 1, 1) and date_range.end <= date(2017, 1, 1):
+                zip_date_start = date_range.start.strftime('%d.%m.%Y')
+                zip_date_end = date_range.end.strftime('%d.%m.%Y')
+                period = ""
+                if date_range.start >= date(2014, 1, 1) and date_range.end <= date(2016, 1, 1):
+                    period = "01.01.2014-01.01.2016"
+                elif date_range.start >= date(2016, 1, 1) and date_range.end <= date(2020, 1, 1):
+                    period = "01.01.2016-01.01.2020"
+                elif date_range.start >= date(2020, 1, 1) and date_range.end <= date(9999, 1, 1):
+                    period = "01.01.2020-Настоящее время"
 
                 with ZipFile(directory + filename, "r") as myzip:
                     file_zip_count = len(myzip.namelist())
@@ -68,16 +131,26 @@ class TenderParser():
                         if nameFileXml.endswith(".xml"):
                             str_data_xml = myzip.read(nameFileXml).decode('utf-8')
                             root = ElementTree.fromstring(str_data_xml)
-                            
-                            result_dict = xmltodict.parse(str_data_xml)
 
-                            # Сохранения словаря
-                            collection.insert_one(result_dict)
+                            try:
+                                type_purchase = root[0].tag.split('}')[1]
+
+                                result_dict = self.parse_to_xml(root[0])
+                                marked_dict = DictUtil.dictionary_compression(result_dict)
+
+                                marked_dict["typePurchase"] = type_purchase
+                                marked_dict["period"] = period
+                                marked_dict["zipDateStart"] = zip_date_start
+                                marked_dict["zipDateEnd"] = zip_date_end
+                                with open('names.csv', 'a', newline='', encoding='utf-8') as csvfile:
+                                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';', extrasaction="ignore")
+                                    writer.writerow(marked_dict)
+                                    csvfile.close()
+                            except Exception as err:
+                                print(f"Unexpected {err=}, {type(err)=}")
+                                continue
 
                         print('\033[F\033[K', end='')
                         print(f'Просмотренно zip: {file_index}/{file_count}\t{round(file_index/file_count*100, 2)}%\t|\t{file_zip_index}/{file_zip_count}\t{round(file_zip_index/file_zip_count*100, 2)}%\t|\tВремя скачивания {self.work_timer.days} дней {self.work_timer.hours} часов {self.work_timer.minutes} минут {self.work_timer.seconds} секунд')
 
                 self.work_timer.calculate_time(file_index, file_count)
-                continue
-            else:
-                continue
